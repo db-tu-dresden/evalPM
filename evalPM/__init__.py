dac3783e1271a80d4b6ecec2c067751bdbf775d9
lucas <lucas.woltmann@tu-dresden.de>
2023-10-09 10:46:34 +0200

    chanegs version

--- evalPM/__init__.py
+++ evalPM/__init__.py
@@ -1,5 +1,5 @@
 """This package is a framework for creating, evaluating, and comparing immission models of particulate matter (PM)."""
-__version__ = '0.1'
+__version__ = '0.1.1'
 
 from . import helpers
 from . import features
